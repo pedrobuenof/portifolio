b86f27164e2f086347f5ef15a9fd09c721d4a12e
Pedro Ferreira <devbueno3@gmail.com>
2024-11-07 23:36:08 -0300

    config: Pequena configuração do reset css + reestrutura da camada de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './reset.css'
+import 'normalize.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,12 @@
 import {Routes, Route} from 'react-router-dom';
-import Home from '../pages/Home/home.tsx';
-import Certification from '../pages/Certifications/Certification.tsx';
-import Projects from '../pages/Projects/Project.tsx';
-import About from '../pages/About/About.tsx';
+import Home from './pages/Home/home.tsx';
+import Certification from './pages/Certifications/Certification.tsx';
+import Projects from './pages/Projects/Project.tsx';
+import About from './pages/About/About.tsx';
 
 export default function AppRoutes(){
     return (
+        // Aqui vem a tag de animações do framemotion - animação para transição de páginas/rotas
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/certifications' element={<Certification/>}/>
