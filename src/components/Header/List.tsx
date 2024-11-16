9434d9ee47b48f19a3ec9fc171ab2bb6f6bb0370
Pedro Ferreira <devbueno3@gmail.com>
2024-11-15 22:13:30 -0300

    chore: ajuste para tornar o componente list realmente dinâmico e menos acoplado

--- src/components/Header/List.tsx
+++ src/components/Header/List.tsx
@@ -1,18 +1,20 @@
 import ItemOfList from "./ItemOfList"
 import './navbar.css'
+import '../footer/footer.css'
 
 
 
 interface ListProps{
+    items: string[];
     customClassName?: string;
 }
 
 export default function List(props: ListProps) {
     return (
         <ul className={props.customClassName}>
-            <ItemOfList itemName="About"/>
-            <ItemOfList itemName="Certifications"/>
-            <ItemOfList itemName="Projects"/>
+            {props.items.map((item, index) => (
+                <ItemOfList key={index} itemName={item}/>
+            ))}
         </ul>
     )
 }
--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,37 +1,41 @@
-import List from "./List";
-import { useMediaQuery } from "react-responsive";
-import { useState } from "react";
+import List from './List'
+import { useMediaQuery } from 'react-responsive'
+import { useState } from 'react'
 import './navbar.css'
 
-export default function Navbar(){
-    const isDesktop = useMediaQuery({ minWidth: 768 })
-    const [ isMenuOpen, setIsMenuOpen ]= useState(false)
+export default function Navbar() {
+  const isDesktop = useMediaQuery({ minWidth: 768 })
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen)
-    }
-
-    return (
-        <>
-            {isDesktop ? (
-                <nav className="navbar_desktop">
-                    <List customClassName="list_desktop"/>
-                </nav>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+  
+  return (
+    <>
+      {isDesktop ? (
+        <nav className="navbar_desktop">
+          <List customClassName="list_desktop" items={["About", "Certification", "Projects"]}/>
+        </nav>
+      ) : (
+        <nav className="navbar_mobile">
+          <div className="menu_icon" onClick={toggleMenu}>
+            {isMenuOpen ? (
+              <span className="close_icon">
+                <img src="src/assets/xmark.svg" />
+              </span>
             ) : (
-                <nav className="navbar_mobile">
-                    <div className="menu_icon" onClick={toggleMenu}>
-                        {isMenuOpen ? (
-                            <span className="close_icon"><img src="src/assets/xmark.svg"/></span>
-                        ) : (
-                            <span className="hamburguer_icon"><img src="src/assets/hamburguer1.svg"/></span>
-                        )}
-                    </div>
-
-                    <div className={`menu_list ${isMenuOpen ? "menu_open" : ""}`}>
-                        <List customClassName="list_mobile"/>
-                    </div>
-                </nav>
+              <span className="hamburguer_icon">
+                <img src="src/assets/hamburguer1.svg" />
+              </span>
             )}
-        </>
-    )
+          </div>
+
+          <div className={`menu_list ${isMenuOpen ? 'menu_open' : ''}`}>
+            <List customClassName="list_mobile" items={["About", "Certification", "Projects"]}/>
+          </div>
+        </nav>
+      )}
+    </>
+  )
 }
